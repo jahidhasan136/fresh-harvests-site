98214b1afc4c9df34bd74b7a38d10010dedeac22
jahidhasan136 <jh3495689@gmail.com>
2024-12-06 21:49:15 +0600

    signup or login successfully

--- src/components/modal/SignUpModal.jsx
+++ src/components/modal/SignUpModal.jsx
@@ -36,41 +36,7 @@ const SignUpModal = ({ isOpen, onClose, onSwitchToLogin }) => {
       .then((res) => res.json())
       .then((data) => {
         if (data.success) {
-          // const handleSubmitUtils = ({ email, password }) => {
-          //   fetch(
-          //     "https://api-fresh-harvest.code-commando.com/api/v1/auth/login",
-          //     {
-          //       method: "POST",
-          //       headers: {
-          //         "Content-Type": "application/json",
-          //       },
-          //       body: JSON.stringify({ email, password }), // Pass email and password to API
-          //     }
-          //   )
-          //     .then((res) => {
-          //       if (!res.ok) {
-          //         throw new Error("Failed to log in"); // Handle non-200 responses
-          //       }
-          //       return res.json();
-          //     })
-          //     .then((data) => {
-          //       if (data.success) {
-          //         const token = data.data.token; // Extract the token from the response
-          //         localStorage.setItem("token", token); // Save the token in localStorage
-          //         console.log("Token saved to localStorage:", token);
-          //         alert(data.message); // Notify the user about successful login
-          //         // Redirect or perform further actions
-          //       } else {
-          //         alert(data.message || "Login failed!"); // Notify about any issues
-          //       }
-          //     })
-          //     .catch((error) => {
-          //       console.error("Error during login:", error.message);
-          //       alert("An error occurred. Please try again.");
-          //     });
-          // };
-
-          alert(data.message); // Notify user about successful registration
+          alert(data.message + "please login now"); // Notify user about successful registration
           onClose(); // Close the modal after successful signup
         } else {
           alert(data.message || "Registration failed!");
